6235139e409b6e88ecea26d953feca13b7e9b112
Globson <samuelpedro1999@gmail.com>
2019-04-19 21:44:37 -0300

    Corrigido prints e brechas em scanfs e corrigo erro do kct do -lm

--- LEITURA.c
+++ LEITURA.c
@@ -44,23 +44,30 @@ int ContaCelula(TipoFila Fila){
 }
 
 TipoQtdeTermos* Leitura(TipoArvore *t, int *N){
-  FILE *Arquivo;
+  FILE *Arquivo = NULL;
 	char caracter, caracter2;
 	TipoChave string;
 	char NomeArq[13];
 	TipoFila Fila;
   TipoQtdeTermos *NiAux = NULL;
-	int i, j=0, TAMFila, idDoc, Verificador;
+	int i, j=0, TAMFila, idDoc, Verificador,Verificador2;
 	FFVazia(&Fila);
   do{
-  	printf("Digite o nome do arquivo da seguinte forma: arquivo(N).txt\n");
-  	scanf("%s", &NomeArq);
+    Verificador = 2;
+    Verificador2 = 0;
+    while(Verificador2 == 0){
+  	printf("\nDigite o nome do arquivo da seguinte forma -> 'arquivo(N).txt' :");
+  	scanf("%s",NomeArq);
+    Arquivo = fopen(NomeArq, "r");
+    if(Arquivo == NULL){
+      printf("\n\tErro!Nao foi possivel abrir arquivo!\n");}
+    else{
+      Verificador2 = 1;}}
     (*N)++;
     NiAux = (TipoQtdeTermos *)realloc(NiAux, (*N)*sizeof(TipoQtdeTermos));
   	idDoc = ((int)NomeArq[7]) - 48;
     NiAux[j].iDdOc = idDoc;
     NiAux[j].nTermsDist = 0;
-  	Arquivo = fopen(NomeArq, "r");
   	while(!feof(Arquivo)){
   		fscanf(Arquivo,"%c", &caracter);
   		while((caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)){
@@ -89,8 +96,11 @@ TipoQtdeTermos* Leitura(TipoArvore *t, int *N){
   		}
   	}
   	fclose(Arquivo);
-    printf("Deseja adicionar textos de outro arquivo?\nSim - 1\nNao - 0\n");
+    while(Verificador != 1 && Verificador != 0){
+    printf("\nDeseja adicionar textos de outro arquivo?\n\tSim - 1\n\tNao - 0\nEntre com a opcao desejada:");
     scanf("%d", &Verificador);
+    if(Verificador != 1 && Verificador != 0){
+      printf("\nErro! Opcao invalida!\n");}}
     j++;
   }while(Verificador != 0);
   return NiAux;
@@ -107,10 +117,10 @@ void Busca(TipoArvore t, int N, TipoQtdeTermos *Ni){
   FListVazia(&listaPeso);
   VetorRlva = (TipoRelevancia *)malloc(N*sizeof(TipoRelevancia));
   do{//Começo doWhile
-    printf("Digite o tamanho da palavra que deseja buscar:\n");
+    printf("\nDigite o tamanho da palavra que deseja buscar:");
     scanf("%d", &TAM);
     str = (char *)malloc((TAM+1)*sizeof(char));
-    printf("Digite a palavra que deseja:\n");
+    printf("\nDigite a palavra que deseja:");
     scanf("%s", str);
     Aux = PesquisaNO(str, t);
     free(str);
@@ -124,7 +134,7 @@ void Busca(TipoArvore t, int N, TipoQtdeTermos *Ni){
     }
     free(f);
     free(id);
-    printf("Deseja buscar mais palavras?\nSim - 1\nNao - 0\n");
+    printf("\nDeseja buscar mais palavras?\n\tSim - 1\n\tNao - 0\nEntre com a opcao desejada:");
     scanf("%d", &Verificador);
   }while(Verificador != 0);//fim doWhile
   for(i=1; i<=N; i++){
